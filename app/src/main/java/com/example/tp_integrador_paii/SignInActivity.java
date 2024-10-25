c3d13f9b3718d06adbc221a1160cce44bb6a4c05
Silva-Carlos-Andres <83254908+Silva-Carlos-Andres@users.noreply.github.com>
2024-10-25 14:30:22 -0300

    Resolucion del TK4

--- app/src/main/java/com/example/tp_integrador_paii/SignInActivity.java
+++ app/src/main/java/com/example/tp_integrador_paii/SignInActivity.java
@@ -1,6 +1,9 @@
 package com.example.tp_integrador_paii;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,23 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
+
+        /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });*/
+
+        Button loginButton = findViewById(R.id.btn_signin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+            public void onClick(View v) {
+                // Redirige a MainActivity, donde se muestra el HomeFragment
+                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
