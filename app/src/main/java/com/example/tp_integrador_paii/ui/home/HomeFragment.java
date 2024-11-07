eba0bcd78c416aa6c594a2250a9b7cff4011ab77
Gastón <80988908+gaston5050@users.noreply.github.com>
2024-11-07 20:30:20 -0300

    TK2 al 98%

--- app/src/main/java/com/example/tp_integrador_paii/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/tp_integrador_paii/ui/home/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.example.tp_integrador_paii.ui.home;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.tp_integrador_paii.SignInActivity;
+import com.example.tp_integrador_paii.daoSQLite.DaoHelperEncuesta;
 import com.example.tp_integrador_paii.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -25,13 +30,24 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
+
+        Integer encuestas = contarEncuestas();
+        homeViewModel.setTextValue("ENCUESTAS REALIZADAS: " + encuestas + "/10");
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private Integer contarEncuestas() {
+        SharedPreferences sp = getActivity().getSharedPreferences(SignInActivity.SHARED_PREFERENCES_LOGIN_DATA, MODE_PRIVATE);
+        String username = sp.getString(SignInActivity.SHARED_PREFERENCES_USERNAME, "desconocido");
+        return DaoHelperEncuesta.obtenerPorEncuestador(username,getActivity()).size();
     }
 }
